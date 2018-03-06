2cb55850285e54faf5532ce3680ea1fe39734f08
rosalaradesoft <rosa.lara@desoft.es>
2018-03-06 13:22:00 +0100

    restablec Estilos

--- EntidadesAtc/EntidadesAtc.cs
+++ EntidadesAtc/EntidadesAtc.cs
@@ -54,6 +54,32 @@ namespace EntidadesAtc
         [DataMember]
         public string ImagenAutor { get; set; }
     }
+    public class Company
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Website { get; set; }
+    }
+    public class ElementoMenu
+    {
+        public int IDElementoMenu { get; set; }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<ElementoMenu> Elementos { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Website { get; set; }
+    }
     [DataContract]
     public class Categoria
     {
---
+++ dsASPCAtc.Web/wwwroot/js/sitetop.js
@@ -0,0 +1,8 @@
+﻿function HacerBucle(Elementos, index) {
+    console.log(index);
+    for (i = 0; i < Elementos.length; i++) {
+        console.log(Elementos[i]);
+        console.log("accordionMenu" + index);
+        console.log(document.getElementById("accordionMenu" + index));
+    }
+}
